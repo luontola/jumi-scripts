cd31dae2d0e1ff4183307e75b20687cd183d7e4f
Esko Luontola <esko.luontola@gmail.com>
2013-07-09 17:51:14 +0300

    Must use the "rc-release" goal because "release" requires the presence of a project

--- src/publish/promote-staging.sh
+++ src/publish/promote-staging.sh
@@ -6,18 +6,26 @@ set -x
 
 VERSION=`cat build/version`
 NEXUS_PLUGIN="org.sonatype.plugins:nexus-staging-maven-plugin:1.4.4"
+PROFILE_ID="2801a17694e2ea"
 
+# Upload and close the OSSRH staging repository
 mvn $NEXUS_PLUGIN:deploy-staged-repository \
     -DnexusUrl=https://oss.sonatype.org/ \
     -DserverId=ossrh-releases \
-    -DstagingProfileId=2801a17694e2ea \
+    -DstagingProfileId="$PROFILE_ID" \
     -DrepositoryDirectory=staging \
     -DstagingDescription="$PROJECT_NAME $VERSION"
 
 # TODO: do a smoke test on the closed staging repository before releasing
 
-mvn $NEXUS_PLUGIN:release \
+# The stagingRepositoryId is a required parameter, but "rc-release" won't read
+# it from the properties file (and "release" doesn't work without a project),
+# so we must read it from the properties file ourselves.
+REPOSITORY_ID=`sed -n -r 's/stagingRepository\.id=(\w+)/\1/p' "staging/$PROFILE_ID.properties"`
+
+# Release the OSSRH staging repository
+mvn $NEXUS_PLUGIN:rc-release \
     -DnexusUrl=https://oss.sonatype.org/ \
     -DserverId=ossrh-releases \
-    -DaltStagingDirectory=staging \
+    -DstagingRepositoryId="$REPOSITORY_ID" \
     -DstagingDescription="$PROJECT_NAME $VERSION"
