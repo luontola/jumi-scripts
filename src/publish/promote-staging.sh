3127b2020ff8fe8e52a02af50f6def4fc6376ee9
Esko Luontola <esko.luontola@gmail.com>
2013-07-09 13:57:22 +0300

    Added staging description

--- src/publish/promote-staging.sh
+++ src/publish/promote-staging.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 set -eu
+: ${PROJECT_NAME:?}
 SCRIPTS=`dirname "$0"`
 set -x
 
+VERSION=`cat build/version`
 NEXUS_PLUGIN="org.sonatype.plugins:nexus-staging-maven-plugin:1.4.4"
 
 mvn $NEXUS_PLUGIN:deploy-staged-repository \
     -DnexusUrl=https://oss.sonatype.org/ \
     -DserverId=ossrh-releases \
     -DstagingProfileId=2801a17694e2ea \
-    -DrepositoryDirectory=staging
+    -DrepositoryDirectory=staging \
+    -DstagingDescription="$PROJECT_NAME $VERSION"
 
 # TODO: do a smoke test on the closed staging repository before releasing
 
 #mvn $NEXUS_PLUGIN:release \
 #    -DnexusUrl=https://oss.sonatype.org/ \
 #    -DserverId=ossrh-releases \
-#    -DaltStagingDirectory=staging
+#    -DaltStagingDirectory=staging \
+#    -DstagingDescription="$PROJECT_NAME $VERSION"
