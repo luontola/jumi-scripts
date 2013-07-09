d48506109e06c4099d1e94a89deb66b6ea39bba6
Esko Luontola <esko.luontola@gmail.com>
2013-07-09 13:18:04 +0300

    Upgraded to nexus-staging-maven-plugin

--- src/publish/promote-staging.sh
+++ src/publish/promote-staging.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 set -eu
-: ${PROJECT_NAME:?}
-: ${DEPLOY_USERNAME:?}
-: ${DEPLOY_PASSWORD:?}
 SCRIPTS=`dirname "$0"`
 set -x
 
-VERSION=`cat build/version`
+NEXUS_PLUGIN="org.sonatype.plugins:nexus-staging-maven-plugin:1.4.4"
 
-ruby $SCRIPTS/upload-maven-repository.rb staging https://oss.sonatype.org/service/local/staging/deploy/maven2
+mvn $NEXUS_PLUGIN:deploy-staged-repository \
+    -DnexusUrl=https://oss.sonatype.org/ \
+    -DserverId=ossrh-releases \
+    -DstagingProfileId=2801a17694e2ea \
+    -DrepositoryDirectory=staging
 
-# The POM is not checked out to working directory when this script is run,
-# so we must say manually that which artifact's open staging repository to close.
-# TODO: parameterize project name & groupId
-mvn org.sonatype.plugins:nexus-maven-plugin:2.1:staging-close \
-    --batch-mode \
-    --errors \
-    -Dnexus.url=https://oss.sonatype.org/ \
-    -Dnexus.username="$DEPLOY_USERNAME" \
-    -Dnexus.password="$DEPLOY_PASSWORD" \
-    -Dnexus.groupId=fi.jumi \
-    -Dnexus.artifactId=parent \
-    -Dnexus.version="$VERSION" \
-    -Dnexus.description="$PROJECT_NAME $VERSION"
+# TODO: do a smoke test on the closed staging repository before releasing
+
+#mvn $NEXUS_PLUGIN:release \
+#    -DnexusUrl=https://oss.sonatype.org/ \
+#    -DserverId=ossrh-releases \
+#    -DaltStagingDirectory=staging
