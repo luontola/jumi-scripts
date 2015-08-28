aaf5631b35d864727708ea47b6f15410212f9b44
Esko Luontola <esko.luontola@gmail.com>
2015-08-28 15:13:10 +0000

    Updated Maven serverId

--- src/publish/promote-staging.sh
+++ src/publish/promote-staging.sh
@@ -11,7 +11,7 @@ PROFILE_ID="2801a17694e2ea"
 # Upload and close the OSSRH staging repository
 mvn $NEXUS_PLUGIN:deploy-staged-repository \
     -DnexusUrl=https://oss.sonatype.org/ \
-    -DserverId=ossrh-releases \
+    -DserverId=ossrh-releases-fi.jumi \
     -DstagingProfileId="$PROFILE_ID" \
     -DrepositoryDirectory=staging \
     -DstagingDescription="$PROJECT_NAME $VERSION"
@@ -26,6 +26,6 @@ REPOSITORY_ID=`sed -n -r 's/stagingRepository\.id=(\w+)/\1/p' "staging/$PROFILE_
 # Release the OSSRH staging repository
 mvn $NEXUS_PLUGIN:rc-release \
     -DnexusUrl=https://oss.sonatype.org/ \
-    -DserverId=ossrh-releases \
+    -DserverId=ossrh-releases-fi.jumi \
     -DstagingRepositoryId="$REPOSITORY_ID" \
     -DstagingDescription="$PROJECT_NAME $VERSION"
